2b21c5759f3e401d7302ae00ac88d781a7bc9d35
muhnashir <muhammadnashir9@gmail.com>
2021-10-08 19:36:29 +0700

    first commit

---
+++ data.php
@@ -0,0 +1,96 @@
+<?php
+                 // $t = $arr[$i][
+                    //     array_search($arr[$i], array_column($arr[$i]['inventory_id'],'inventory_id'))
+                    // ];
+
+    function allElectronic($arr, $string){
+        $tmp=[];
+        if($string=="electronic"){
+            for($i = 0; $i<count($arr); $i++){
+                if($arr[$i]["type"]==$string){             
+                  array_push($tmp, $arr[$i]["name"]);
+                }           
+            }
+        }elseif($string=="furniture"){
+            for($i = 0; $i<count($arr); $i++){
+                if($arr[$i]["type"]==$string){             
+                  array_push($tmp, $arr[$i]);
+                }           
+            }
+        }
+        
+        echo json_encode($tmp);
+    }
+
+    function color($arr, $string){
+        $tmp=[];
+        for($i = 0; $i<count($arr); $i++){
+                for($j=0; $j<count($arr[$i]["tags"]); $j++){
+                    if($arr[$i]["tags"][$j]==$string){             
+                        array_push($tmp, $arr[$i]);
+                    }
+                }           
+        }
+        if($tmp){
+            echo json_encode($tmp);
+        }else{
+            echo "not found";
+        }
+    }
+
+    function finById($arr, $string){
+        $tmp=[];
+        for($i = 0; $i<count($arr); $i++){
+            if($arr[$i]["inventory_id"]==$string){             
+                array_push($tmp, $arr[$i]);
+            }        
+        }
+        if($tmp){
+            echo json_encode($tmp);
+        }else{
+            echo "not found";
+        }
+    }
+
+    function findByDate($arr, $date){
+        $tanggal = date("Y-m-d", strtotime($date));
+        $format = strtotime($tanggal);
+        echo $format;
+        die;
+        $tmp=[];
+        for($i = 0; $i<count($arr); $i++){
+            if($arr[$i]["purchased_at"]==$format){             
+                array_push($tmp, $arr[$i]);
+            }        
+        }
+        if($tmp){
+            echo json_encode($tmp);
+        }else{
+            echo "not found";
+        }
+        
+    }
+
+    
+    $str = file_get_contents('data.json');
+    $json_arr = json_decode($str, true);
+
+    // Find all electronic devices.
+    echo allElectronic($json_arr, "electronic");
+    echo "<br>";
+    // Find all the furniture.
+    echo allElectronic($json_arr, "furniture");
+    echo "<br>";
+    // Find all items with brown color.
+    echo color($json_arr, "brown");
+    echo "<br>";
+    // Find items in the Meeting Room.
+    echo finById($json_arr, "2932");
+    echo "<br>";
+    // Find all items were purchased on 16 Januari 2020.
+    echo findByDate($json_arr, "10 december 2021");
+    echo "<br>";
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+function kabisat($tahun1, $tahun2){
+    if($tahun1%4==0 && $tahun2%4==0){
+        for ($i=$tahun1; $i<=$tahun2; $i+=4){
+            if($i!=$tahun1){
+                echo $i. "<br>";
+            }
+        }
+    }else{
+        echo "tahun yang anda masukkan bukan tahun kabisat";
+    }
+}
+
+function palindrome($string){
+    $toLower = strtoupper($string);
+    $reverse = strrev($toLower);
+    if($toLower == $reverse){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+function reverse($kalimat){
+    $pecahString = explode(" ",$kalimat);
+    $i=1;
+    foreach($pecahString as $p){
+        $re = strrev($p);
+        echo $re." ";
+    }
+}
+
+function fibonaci($arr){
+    $tmp=[];
+    $tm;
+
+    for($i=0; $i<count($arr)+2; $i++){
+        if($i<count($arr)){
+            array_push($tmp, $arr[$i]);
+        }elseif($i==4){
+            $tm = $tmp[$i-1]+2;
+            array_push($tmp, $tm);
+        }
+        else{
+            $tm = $arr[$i-1]+$arr[$i-2]+$arr[$i-3];
+            array_push($tmp, $tm);
+        }
+    }
+
+    return $tmp;
+}
+
+function FizzBuzz($n){
+    $output=[];
+    for($i=1; $i<=$n; $i++){
+        if($i%3==0){
+            array_push($output,"Fizz ");
+        }elseif($i%5==0){
+            array_push($output,"Buzz ");
+        }else{
+            array_push($output,$i." ");
+        }
+    }
+    return $output; 
+}
+
+
+
+$tahun1=1900;
+$tahun2=2000;
+// echo kabisat($tahun1, $tahun2);
+
+$string="Ibu Ratna antar ubi";
+// echo palindrome($string);
+
+$kalimat= "I am A Great human";
+// echo reverse($kalimat);
+
+$arr=[15,1,3];
+echo implode(" ", fibonaci($arr));
+
+
+$n=10;
+// echo implode("", FizzBuzz($n));
+
+?>
